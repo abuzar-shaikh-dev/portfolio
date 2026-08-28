2605f340f166d4c0ac020b03a078225cc4a6eb4c
Abuzar <tracbyte.tech@gmail.com>
2026-08-28 19:21:01 +0530

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react' // or your framework plugin
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Replace with your repository name if different
+  base: '/portfolio/',
 })
